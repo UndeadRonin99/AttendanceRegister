7cdaafbcc1014b995d94bb87afe68cd62805c5de
UndeadRonin99 <104002258+UndeadRonin99@users.noreply.github.com>
2024-10-08 21:55:13 +0200

    hardcoded server IP

--- sample_register.py
+++ sample_register.py
@@ -13,7 +13,7 @@ def create_qr_code(data, filename):
 # Generate a QR code containing the URL to invoke the attendance function
 # This could be hosted on a server, which when accessed will call `update_attendance`
 server_ip = '0.0.0.0'
-qr_data = f'http://{server_ip}:5000/record-attendance'
+qr_data = f'http://0.0.0.0:5000/record-attendance'
 create_qr_code(qr_data, 'sampleAttendance.png')
 
 # Initialize Firebase Admin SDK
